9006ddd60b2a1ef832ff7d15e85391f69ed5e2e4
Vinesh <92975459+Vineshvinu@users.noreply.github.com>
2023-01-20 23:40:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -228,7 +228,7 @@ Aᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:
 • /plink - <code>Jᴜsᴛ ʟɪᴋᴇ /link, ʙᴜᴛ ᴛʜᴇ ғɪʟᴇ ᴡɪʟʟ ʙᴇ sᴇɴᴅ ᴡɪᴛʜ ғᴏʀᴡᴀʀᴅ ʀᴇsᴛʀɪᴄᴛɪᴏɴ.</code>"""
 
     RESTART_TXT = """
-<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ ! My owner @Vinesh_vinu
+<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ ! **My owner @Vinesh_vinu**
 
 📅 Dᴀᴛᴇ : <code>{}</code>
 ⏰ Tɪᴍᴇ : <code>{}</code>
